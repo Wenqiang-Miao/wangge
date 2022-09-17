5621dc9e37569201933c3055a9650d33b8f3e45c
MiaoWenqiang <2546577694@qq.com>
2022-09-17 21:15:37 +0800

    数据天价买入是否成交标志

--- utils/ana_utils.py
+++ utils/ana_utils.py
@@ -10,3 +10,23 @@
 512980_chuanmei
     
 """
+
+
+def get_everage_cost():
+    """
+    # 计算平均的持仓成本
+    """
+    return
+
+
+def get_buy_info():
+    """
+    获取买入相关的信息
+    """
+
+
+
+
+if __name__ == '__main__':
+    pass
+
