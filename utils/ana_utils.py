78acf116e9b647982aba34d18fdf235f86b40643
MiaoWenqiang <2546577694@qq.com>
2022-09-18 22:22:11 +0800

    添加半路上车计算方法

--- utils/ana_utils.py
+++ utils/ana_utils.py
@@ -10,24 +10,82 @@
 512980_chuanmei
     
 """
-
+import pandas as pd
+import numpy as np
+import os
+import sys
 
 
 
 
 
 def get_list_by_index(file_pth='./data/159938_yiyao.csv', index='买入价'):
+    """
+    按照index返回csv数据中的内容
+    Args:
+        file_pth (str, optional): _description_. Defaults to './data/159938_yiyao.csv'.
+        index (str, optional): _description_. Defaults to '买入价'.
+
+    Returns:
+        _type_: _description_
+    """
+    raw_df = pd.read_csv(file_pth)
+    # 完成对None值的替换，并将数据转换为float型
+    raw_df = raw_df.replace("None", 0.0).astype(np.float64)
+
+    #res_array = raw_df[index]
+    if index in ['入股数', '出股数', '买入是否成交']:
+        raw_df[index] = raw_df[index].astype(np.int32)
+    res_array = list(raw_df[index])
     
+    #print(list(res_array))
+    return res_array
 
-    return
 
 
 
-def get_everage_cost():
+def get_everage_cost(file_pth='./data/159938_yiyao.csv'):
     """
     # 计算平均的持仓成本
     """
-    return
+    avg_cost = 0
+    buy_money_li = get_list_by_index(file_pth=file_pth, index='买入金额')  
+    buy_amount_li = get_list_by_index(file_pth=file_pth, index='入股数') 
+    # 判断买入是否成交的
+    done_flg_li = get_list_by_index(file_pth=file_pth, index='买入是否成交') 
+    
+    # 确定成交数量，持仓成本只更新已成交部分
+    done_num = 0
+    for done_flg in done_flg_li:
+        if done_flg == 0:
+            break
+        done_num += 1
+    
+    total_money = sum(buy_money_li[:done_num])
+    total_amount = sum(buy_amount_li[:done_num])
+    if total_amount != 0:
+        avg_cost = total_money / total_amount    
+    
+    print('{}:总入股数{}, 买入总金额{}, 持仓成本{}'.format(
+        os.path.basename(file_pth), total_amount, total_money, avg_cost) )
+    
+    return avg_cost
+
+
+
+def batch_get_everage_cost(file_dir='./data/'):
+    data_li=['159938_yiyao.csv',
+             '159920_hengsheng-etf.csv', 
+             '513180_hengshengkeji.csv',
+             '512880_zhengquan.csv',
+             '515180_100hongli.csv', 
+             '519280_chuanmei.csv'
+             ]
+    for file_name in data_li:
+        if file_name.endswith('.csv'):
+            #print(file_name)
+            get_everage_cost(os.path.join(file_dir, file_name))
+        
 
 
 def get_buy_info():
@@ -39,6 +97,19 @@ def get_buy_info():
 
 
 
+def banlu_shangche():
+    """
+    根据已经买入的成本，决定如何半路上车
+    
+    """
+    
+    return
+
+
+
+
 if __name__ == '__main__':
-    pass
+    batch_get_everage_cost()
+    
+    print('')
 
--- wangge_e.py
+++ wangge_e.py
@@ -7,6 +7,8 @@ import pandas as pd
 import numpy as np
 import math
 
+from utils.ana_utils import get_list_by_index
+
 class DefaultCongfig(object):
     """
     一些全局参数的保存
@@ -112,7 +114,8 @@ def get_my_stratdgey(data_dir='./data/', dst_dir='./dst_stratgey/'):
              '515180_100hongli.csv', 
              '519280_chuanmei.csv'
              ]
-    scale_ratio_li = [0.16, 0.17, 0.2, 0.17, 0.16, 0.2]
+    #scale_ratio_li = [0.16, 0.17, 0.2, 0.17, 0.16, 0.2]
+    scale_ratio_li = [0.08, 0.085, 0.1, 0.085, 0.08, 0.01]
     
     assert len(data_li) == len(scale_ratio_li)
     for idx, data_file_name in enumerate(data_li):
@@ -122,11 +125,93 @@ def get_my_stratdgey(data_dir='./data/', dst_dir='./dst_stratgey/'):
 
 
 
+def banlu_shangche(data_pth='./dst_stratgey/159938_yiyao.csv', cur_price=0.724, dst_dir='./banlu_shangche/'):
+    """
+    根据当前价格cur_price，确定半路上车的买入量；并根据价格，更新需要设置的卖出参数
+    网格价格高于cur_price的，求策略中所有的买入量，然后以cur_price买入这么多，并按照相同的参数设置卖出
+    Args:
+        data_pth (str, optional): _description_. Defaults to './data/159938_yiyao.csv'.
+        cur_price (float, optional): _description_. Defaults to 0.98.
+    """
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    
+    raw_df = read_raw_data(data_pth=data_pth)
+    #print(raw_df)
+    #print(raw_df['买入价'][0])
+    
+    buy_price_li = get_list_by_index(file_pth=data_pth, index='买入价') # 以列表方式返回对应值
+    buy_amount_li = get_list_by_index(file_pth=data_pth, index='入股数') 
+    # 判断买入是否成交的
+    done_flg_li = get_list_by_index(file_pth=data_pth, index='买入是否成交') 
+    
+    # 确定成交数量，持仓成本只更新已成交部分
+    done_num = 0
+    for done_flg in done_flg_li:
+        if done_flg == 0:
+            break
+        done_num += 1
+    # 用于判断，哪个买入价格的索引是高于cur_price的
+    shangche_head_ind = -1
+    for idx in range(done_num):
+        if buy_price_li[idx] > cur_price:
+            shangche_head_ind = idx
+    # 网格高于当前价格的部分，才是要半路上车的买入量
+    shangche_amount = 0
+    if shangche_head_ind > 0:
+        shangche_amount = sum(buy_amount_li[:shangche_head_ind+1])
+    
+    total_money = shangche_amount * cur_price
+    
+    print('#'*20, os.path.basename(data_pth), '#'*20)
+    res_str = '卖出触发价,卖出价,出股数,卖出金额\n'
+    print('当前价格{},共买入{}份，总价{}元;设置卖出：'.format(cur_price, shangche_amount, total_money))
+    for i in range(shangche_head_ind+1):
+        
+        print('\t卖出触发价{:*>6.3f},卖出价{:*>6.3f},出股数{:*>8.1f},卖出金额{:*>8.1f}'.format(raw_df['卖出触发价'][i], 
+                        raw_df['卖出价'][i], raw_df['出股数'][i], raw_df['卖出金额'][i]))
+        res_str += "{:.3f},{:.3f},{:d},{:.1f}\n".format(raw_df['卖出触发价'][i], 
+                        raw_df['卖出价'][i], int(raw_df['出股数'][i]), raw_df['卖出金额'][i])
+    #print('#'*8, os.path.basename(data_pth), '#'*8)
+    
+    with open(os.path.join(dst_dir, os.path.basename(data_pth)), 'w', encoding='utf-8') as f2_obj:
+        f2_obj.write(res_str)
+    
+            
+  
+def batch_banlu_shangche(data_dir='./dst_stratgey/', dst_dir='./banlu_shangche/'): 
+    """
+    对半路上车的进一步封装
+    Args:
+        data_dir (str, optional): _description_. Defaults to './dst_stratgey/'.
+    """
+    data_li=['159938_yiyao.csv',
+             '159920_hengsheng-etf.csv', 
+             '513180_hengshengkeji.csv',
+             '512880_zhengquan.csv',
+             '515180_100hongli.csv', 
+             '519280_chuanmei.csv'
+             ]
+    cur_price_li = [0.724, 1.071, 0.519, 0.869, 1.288, 0.565]
+    
+    assert len(data_li) == len(cur_price_li)
+    
+    for idx, data_file in enumerate(data_li):
+        data_pth = os.path.join(data_dir, data_file)
+        banlu_shangche(data_pth=data_pth, cur_price=cur_price_li[idx], dst_dir=dst_dir)
+
 
 
 if __name__ == '__main__':
+    # 使用方法：
+    # 1.先用get_my_stratdgey 获取自己的策略，记得按照自己的资金，更改scale_ratio_li 内容
+    # 2.再调用batch_banlu_shangche，确定半路上车买入的情况，以及设置的卖出结果
     
-    get_my_stratdgey()
+    raw_data_dir = './data/'
+    my_stratdgey_dir = './dst_stratgey/'
+    shangche_dir = './banlu_shangche/'
+    # get_my_stratdgey(data_dir=raw_data_dir, dst_dir=my_stratdgey_dir)
+    # batch_banlu_shangche(data_dir=my_stratdgey_dir, dst_dir=shangche_dir)
     
     
     print('')
